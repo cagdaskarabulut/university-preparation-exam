5efe11577b2dfbd517e98232169b7f84dcaf5234
cagdaskarabulut <cagdas.karabulut@gmail.com>
2023-08-14 11:25:08 +0300

    I started to write on answer selected function

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -3,35 +3,119 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Timers;
 
 public class Quiz : MonoBehaviour
 {
+  //! Settings 
+  [Header("Mode Selection")]
+  //- Mode
+  [SerializeField] bool isManuelSelection = false;
+  [SerializeField] bool isShortTermQuizAutoSelection = false;
+  [SerializeField] bool isLongTermQuizAutoSelection = true;
+
+  [Header("Manuel Selection Details")]
+  //- Next and Previous Buttons (Changing from Interface or Quiz Mode)
+  [SerializeField] bool isShowNextAndPreviousButtons = true;
+  //- Timer For Question (Changing from Interface or Quiz Mode)
+  [SerializeField] bool isEnabledTimerForQuestion = false;
+  [SerializeField] Image timerImageForQuestion;
+  private Timer timerForQuestion;
+  //- Timer For Quiz (Changing from Interface or Quiz Mode)
+  [SerializeField] bool isEnabledTimerForQuiz = true;
+  [SerializeField] Slider timerSliderForQuiz;
+  private Timer timerForQuiz;
+  //- Page Components
+  private RectTransform rectForQuestion;
+  private GameObject[] routingButtons;
+
   [Header("Admin Settings")]
-  [SerializeField] bool randomQuestions = false;
-  [SerializeField] bool checkAnswerLast = true;
-  [SerializeField] bool autoGetNextQuestion = true;
+  //- Admin Settings 
+  [SerializeField] bool isRandomQuestions = false; //_ Changing from Interface
+  [SerializeField] bool isCheckAnswersAtFinal = true; //_Changing from Interface or Quiz Mode
+  [SerializeField] bool isGetNextQuestionAutomatically = true; //_Changing from Interface or Quiz Mode
+
+  //! Values 
+  [SerializeField] int totalScore = 0;
 
   [Header("Questions")]
+  //- Questions
   [SerializeField] TextMeshProUGUI questionText;
   [SerializeField] List<QuestionObject> questions = new List<QuestionObject>();
-  int nextQuestionIndex = 0;
-  QuestionObject currentQuestion;
+  private int nextQuestionIndex = 0;
+  private QuestionObject currentQuestion;
+  private int totalQuestionsCount = 0;
 
   [Header("Answers")]
+  //- Answers
   [SerializeField] GameObject[] answerButtons;
-  int correctAnswerIndex;
-  bool hasAnsweredEarly = true;
+  [SerializeField] Sprite defaultAnswerButtonSprite;
+  [SerializeField] Sprite correctAnswerButtonSprite;
+  private int correctAnswerIndex;
+  private Dictionary<int, int> allAnswers; //_ questionIndex, resultIndex
 
-  [Header("Button Colors")]
-  [SerializeField] Sprite defaultAnswerSprite;
-  [SerializeField] Sprite correctAnswerSprite;
+  void SetupComponentsBeforeStart()
+  {
+    timerImageForQuestion = GameObject.Find("RemainingTimeForQuestion").GetComponent<Image>();
+    timerSliderForQuiz = GameObject.Find("RemainingTimeForQuizSlider").GetComponent<Slider>();
+    routingButtons = GameObject.FindGameObjectsWithTag("RoutingButtons");
+    rectForQuestion = GameObject.Find("QuestionScrollView").GetComponent<RectTransform>();
 
-  int totalQuestionsCount = 0;
-  Dictionary<int, int> allAnswers; //questionIndex, resultIndex
-  int score = 0;
+    if (isManuelSelection || (!isShortTermQuizAutoSelection && !isLongTermQuizAutoSelection))
+    {
+      timerImageForQuestion.enabled = isEnabledTimerForQuestion;
+      timerSliderForQuiz.gameObject.SetActive(isEnabledTimerForQuiz);
+    }
+    else if (isShortTermQuizAutoSelection)
+    {
+      isEnabledTimerForQuestion = true;
+      timerImageForQuestion.enabled = isEnabledTimerForQuestion;
+
+      isEnabledTimerForQuiz = false;
+      timerSliderForQuiz.gameObject.SetActive(isEnabledTimerForQuiz);
+
+      isShowNextAndPreviousButtons = false;
+      foreach (GameObject routingButton in routingButtons)
+      {
+        routingButton.gameObject.SetActive(false);
+      }
+
+      isCheckAnswersAtFinal = false;
+
+      isGetNextQuestionAutomatically = true;
+
+    }
+    else if (isLongTermQuizAutoSelection)
+    {
+      isEnabledTimerForQuestion = false;
+      timerImageForQuestion.enabled = isEnabledTimerForQuestion;
+
+      isEnabledTimerForQuiz = true;
+      timerSliderForQuiz.gameObject.SetActive(isEnabledTimerForQuiz);
+
+      isShowNextAndPreviousButtons = true;
+      foreach (GameObject routingButton in routingButtons)
+      {
+        routingButton.gameObject.SetActive(true);
+      }
+
+      isCheckAnswersAtFinal = true;
+
+      isGetNextQuestionAutomatically = false;
+    }
+
+    PrepareRoutingButtons(isShowNextAndPreviousButtons);
+
+  }
 
   void Start()
   {
+    SetupComponentsBeforeStart();
+    //timerForQuestion = FindObjectOfType<Timer>();
+    //timerImageForQuestion.fillAmount = timerForQuestion.fillFraction;
+    //timerForQuiz = FindObjectOfType<Timer>();
+    //timerSliderForQuiz.fillAmount = timerForQuiz.fillFraction;
+
     totalQuestionsCount = questions.Count;
     allAnswers = new Dictionary<int, int>();
     GetQuestion();
@@ -59,13 +143,37 @@ public class Quiz : MonoBehaviour
     //     }
   }
 
+  void PrepareRoutingButtons(bool isShowNextAndPreviousButtons)
+  {
+    if (isShowNextAndPreviousButtons)
+    {
+      foreach (GameObject routingButton in routingButtons)
+      {
+        routingButton.gameObject.SetActive(true);
+      }
+    }
+    else
+    {
+      foreach (GameObject routingButton in routingButtons)
+      {
+        routingButton.gameObject.SetActive(false);
+      }
+    }
+
+    if (!isShowNextAndPreviousButtons)
+    {
+      rectForQuestion.offsetMax = new Vector2(rectForQuestion.offsetMax.x, -350);
+    }
+  }
+
   void GetQuestion()
   {
     if (questions.Count > 0)
     {
+      //timerImageForQuestion.fillAmount = timer.fillFraction;
       // SetButtonState(true);
       // SetDefaultButtonSprites();
-      if (randomQuestions)
+      if (isRandomQuestions)
       {
         GetRandomQuestion();
       }
@@ -73,10 +181,11 @@ public class Quiz : MonoBehaviour
       {
         GetNextQuestion();
       }
-
       DisplayQuestion();
       // progressBar.value++;
       // scoreKeeper.IncrementQuestionsSeen();
+
+      //TODO Seçim yaptığında question sayacı ve resmi sıfırla
     }
   }
 
@@ -134,24 +243,31 @@ public class Quiz : MonoBehaviour
 
   public void onAnswerSelected(int index)
   {
+    //TODO Seçim yaptığında doğru şıkkı işaretle 
+    
 
+    //TODO 1- Seçim yaptığında hızlı moddaysa seçimden birkaç saniye sonraya kadar ses efekti çıkartıp 
+    //TODO 2- sonunda doğru şıkkı farklı renkle işaretle , eğer doğru şıkkı seçmişse şıkkı yeşil yap 
+    //TODO 3- değilse kırmızı yap ve doğru şıkkı yeşil yap
+
+    //- Mevcut sorunun cevabını listeye ekle
     allAnswers.Add(nextQuestionIndex - 1, index);
 
-    if (!checkAnswerLast)
+    //- Seçim yaptığında anlık puan hesaplanması seçiliyse, puan hesaplamasını yap 
+    if (!isCheckAnswersAtFinal)
     {
       if (index == currentQuestion.GetCorrectAnswerIndex())
       {
-        score = score + ((1 / totalQuestionsCount) * 100);
+        totalScore += ((1 / totalQuestionsCount) * 100);
       }
     }
 
-    if (autoGetNextQuestion)
+    //- Seçim yaptığında otomatik sıradaki soruyu getir ayarı seçiliyse otomatik yeni soruyu getir
+    if (isGetNextQuestionAutomatically)
     {
       GetQuestion();
     }
 
   }
-
-
-
 }
+
